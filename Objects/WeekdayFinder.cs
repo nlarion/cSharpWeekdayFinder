191503ae0fc3dbc96721aa8bed878af25dc32c51
alex <alex>
2016-02-18 13:30:08 -0800

    Finish up methods to pass tests

--- Objects/WeekdayFinder.cs
+++ Objects/WeekdayFinder.cs
@@ -5,58 +5,71 @@ using System.Linq;
 namespace LeapYearNS.Objects
 {
   public class WeekdayFinder
+  {
+    private int _month;
+    private int _day;
+    private int _year;
+    private int _totalNumber;
+    private int _TotalLeapYears;
+
+    public WeekdayFinder (int month, int day, int year)
     {
-        private int _month;
-        private int _day;
-        private int _year;
-        private int _totalNumber;
-        private int _TotalLeapYears;
+      _month = month;
+      _day = day;
+      _year = year;
+    }
 
-        public WeekdayFinder (int month, int day, int year)
+    public int NumberOfLeapYears()
+    {
+      int leapYear = 0;
+      for(var i = 1; i<=_year;i++)
+      {
+        if(IsLeapYear(i))
         {
-          _month = month;
-          _day = day;
-          _year = year;
+          leapYear++;
         }
+      }
+      _TotalLeapYears = leapYear;
+      return leapYear;
+    }
 
-        public int NumberOfLeapYears()
-        {
-          int leapYear = 0;
-          for(var i = 1; i<=_year;i++)
-          {
-            if(i % 4==0)
-            {
-              leapYear++;
-              if(i%100==0&&i%400!=0)
-              // if(i%100==0)
-              {
-                leapYear--;
-              }
-            }
-          }
-          _TotalLeapYears = leapYear;
-          return leapYear;
-        }
+    public bool IsLeapYear(int year)
+    {
+      if(year % 400 == 0 )
+      {
+        return true;
+      }
+      else if (year % 4 == 0 && year % 100 !=0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
 
-        public int FindNumberFromDate()
-        {
-          int[] monthArray = new int[] {0,31,59,90,120,151,181,212,243,273,304,334};
-          _totalNumber = _day+monthArray[_month-1]+((_year-1)*365) + _TotalLeapYears;
-          Console.WriteLine(_totalNumber);
-          Console.WriteLine(_TotalLeapYears);
+    public int FindNumberFromDate()
+    {
+      int[] monthArray;
+      if(IsLeapYear(_year))
+      {
+        monthArray = new int[] {0,31,60,91,121,152,182,213,244,274,305,335};
+        _totalNumber = _day + monthArray[_month-1] + ((_year-1)*365) + _TotalLeapYears;
+      }
+      else
+      {
+        monthArray = new int[] {0,31,59,90,120,151,181,212,243,273,304,334};
+        _totalNumber = _day + monthArray[_month-1] + ((_year-1)*365) + _TotalLeapYears+1;
+      }
+      return _totalNumber;
+    }
 
-          return _totalNumber;
-        }
-
-        public string FindDayOfWeek()
-        {
-
-          string[] daysArray = new string[]{"Saturday","Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-          // _dayOfWeek = daysArray;
-
-          int remainder = (_totalNumber) % 7;
-          Console.WriteLine("remainder:"+remainder);
-          return daysArray[remainder];
-        }
+    public string FindDayOfWeek()
+    {
+      string[] daysArray = new string[]{"Saturday","Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+      int remainder = (_totalNumber) % 7;
+      return daysArray[remainder];
+    }
   }
 }
--- Tests/WordCountTest.cs
+++ Tests/WordCountTest.cs
@@ -9,59 +9,74 @@ namespace LeapYearTestNS
 {
   public class WeekdayFinderTest
   {
-    // [Fact]
-    // public void TestGetNumberOfDaysFromMonthAndDay()
-    // {
-    //   WeekdayFinder weekdayFinder = new WeekdayFinder(7,11,1);
-    //   Assert.Equal(192, weekdayFinder.FindNumberFromDate());
-    // }
+    [Fact]
+    public void TestGetNumberOfDaysFromMonthAndDay()
+    {
+      WeekdayFinder weekdayFinder = new WeekdayFinder(7,11,1);
+      Assert.Equal(193, weekdayFinder.FindNumberFromDate());
+    }
 
     [Fact]
     public void GetDayOfTheWeekFromNumberOFDays()
     {
-      WeekdayFinder weekdayFinder = new WeekdayFinder(7,11,100);
+      WeekdayFinder weekdayFinder = new WeekdayFinder(3,1,2016);
       weekdayFinder.NumberOfLeapYears();
       weekdayFinder.FindNumberFromDate();
-      Assert.Equal("Saturday", weekdayFinder.FindDayOfWeek());
+      DateTime dateValue = new DateTime(2016, 3, 1);
+      Assert.Equal(dateValue.ToString("dddd"), weekdayFinder.FindDayOfWeek());
     }
-
+    [Fact]
+    public void GetDayOfTheWeekFromNumberOFDays1()
+    {
+      WeekdayFinder weekdayFinder = new WeekdayFinder(2,1,2015);
+      weekdayFinder.NumberOfLeapYears();
+      weekdayFinder.FindNumberFromDate();
+      DateTime dateValue = new DateTime(2015, 2, 1);
+      Assert.Equal(dateValue.ToString("dddd"), weekdayFinder.FindDayOfWeek());
+    }
     [Fact]
     public void GetDayOfTheWeekFromNumberOFDays2()
     {
-      WeekdayFinder weekdayFinder = new WeekdayFinder(1,1,1);
+      WeekdayFinder weekdayFinder = new WeekdayFinder(3,1,2014);
       weekdayFinder.NumberOfLeapYears();
-
       weekdayFinder.FindNumberFromDate();
-      Assert.Equal("Saturday", weekdayFinder.FindDayOfWeek());
+      DateTime dateValue = new DateTime(2014, 3, 1);
+      Assert.Equal(dateValue.ToString("dddd"), weekdayFinder.FindDayOfWeek());
     }
-
     [Fact]
     public void GetDayOfTheWeekFromNumberOFDays3()
     {
-      WeekdayFinder weekdayFinder = new WeekdayFinder(7,11,200);
+      WeekdayFinder weekdayFinder = new WeekdayFinder(3,2,2000);
       weekdayFinder.NumberOfLeapYears();
-
       weekdayFinder.FindNumberFromDate();
-      Assert.Equal("Friday", weekdayFinder.FindDayOfWeek());
+      DateTime dateValue = new DateTime(2000, 3, 2);
+      Assert.Equal(dateValue.ToString("dddd"), weekdayFinder.FindDayOfWeek());
+    }
+    [Fact]
+    public void GetDayOfTheWeekFromNumberOFDays4()
+    {
+      WeekdayFinder weekdayFinder = new WeekdayFinder(2,29,2000);
+      weekdayFinder.NumberOfLeapYears();
+      weekdayFinder.FindNumberFromDate();
+      DateTime dateValue = new DateTime(2000, 2, 29);
+      Assert.Equal(dateValue.ToString("dddd"), weekdayFinder.FindDayOfWeek());
+    }
+    [Fact]
+    public void GetDayOfTheWeekFromNumberOFDays5()
+    {
+      WeekdayFinder weekdayFinder = new WeekdayFinder(2,28,2000);
+      weekdayFinder.NumberOfLeapYears();
+      weekdayFinder.FindNumberFromDate();
+      DateTime dateValue = new DateTime(2000, 2, 28);
+      Assert.Equal(dateValue.ToString("dddd"), weekdayFinder.FindDayOfWeek());
+    }
+    [Fact]
+    public void CountTheLeapYears()
+    {
+      WeekdayFinder weekdayFinder = new WeekdayFinder(7,11,1000);
+      Assert.Equal(242, weekdayFinder.NumberOfLeapYears());
     }
 
-    // [Fact]
-    // public void GetDayOfTheWeekFromNumberOFDays4()
-    // {
-    //   WeekdayFinder weekdayFinder = new WeekdayFinder(7,11,8);
-    //   weekdayFinder.NumberOfLeapYears();
-    //
-    //   weekdayFinder.FindNumberFromDate();
-    //   Assert.Equal("Wednesday", weekdayFinder.FindDayOfWeek());
-    // }
-
-    // [Fact]
-    // public void CountTheLeapYears()
-    // {
-    //   WeekdayFinder weekdayFinder = new WeekdayFinder(7,11,1000);
-    //   Assert.Equal(242, weekdayFinder.NumberOfLeapYears());
-    // }
-    //
 
   }
 }
